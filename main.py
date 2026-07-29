61954202eecc09627ed789821e02c1204d060dc0
jxsuelwa-svg <jxsuelwa@gmail.com>
2026-07-29 13:40:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
- import requests
+import requests
 from telegram import uuid
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
